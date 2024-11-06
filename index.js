3e9f074d6a9f3359eb8ccf6184911bd16d4482c5
nemecro <nemecstudies@gmail.com>
2024-11-06 16:39:40 +0100

    feat: create a Book constructor, add title, author, pages and read properties, add info method

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log('Hello, world!');
+function Book(title, author, pages, read){
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+    this.info = function(){
+        if (read === 'yes'){
+            return `${this.title} by ${this.author}, ${this.pages} pages, already read`;
+        } else {
+            return `${this.title} by ${this.author}, ${this.pages} pages, not read yet`;
+        }
+    }
+}
